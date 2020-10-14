b622545ddc43b50e030ab0d396788a0c3c0afc7e
koorosh <59454315+zerodey@users.noreply.github.com>
2020-10-14 16:29:01 -0400

    Add files via upload

---
+++ Training/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+var x = 4;
+
+
+
+switch (new Date().getDay()) {
+    case 0:
+        alert("sunday");
+        break;
+    case 1:
+        alert("Monday");
+        break;
+    case 3:
+        alert("Wednesday");
+        break;
+
+    default:
+        alert("None");
+        break;
+}
